8880fb412485b190bf227e74b0b62b16bb57d1e6
Nathan Vinel <nathanvinel20@gmail.com>
2025-04-10 20:18:58 +0200

    last

--- nchiffres.py
+++ nchiffres.py
@@ -1,35 +1,25 @@
-from itertools import product
 import time
 
-
-def n_chiffres(n, f):
+def n_chiffres(n,f):
     start = time.time()
-    chiffres = [str(i) for i in range(10)]
 
     fichier = open(f, "r")
-    decimales = fichier.read()
+    d = fichier.read()
     fichier.close()
 
     lst = []
-    for i in range(len(decimales)-n+1):
+    for i in range(len(d)-n+1):
         l = ""
         for j in range(n):
-            l += str(decimales[i+j])
-        lst.append(l)
-    
+            l += str(d[i+j])
+        if not l in lst:
+            print(l)
+            lst.append(l)
 
-    c= 0
-    for elt in product(chiffres, repeat=n):
-        e = ""
-        for i in range(n):
-            e += str(elt[i])
-    
-        if e in lst:
-            c += 1
+        
+    print(f"durée d'execution : {time.time() - start}")
+
+    return len(lst)
 
 
-    print(time.time() - start)
-
-    return c
-
-
+print(n_chiffres(5, "carree_1M.txt"))
